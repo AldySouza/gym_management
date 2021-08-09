108becec576d8b7af0e183e5cdbf11fb6d02c3e7
esxadm <aldicleiry@hotmail.com>
2021-08-09 14:03:40 -0300

    tests implementedtests implemented

---
+++ test/wabanex/user_test.exs
@@ -0,0 +1,31 @@
+defmodule Wabanex.UserTest do
+  use Wabanex.DataCase, async: true
+
+  alias Wabanex.User
+
+  describe "changeset/1" do
+    test "when all params are valid, returns a valid changeset" do
+      params = %{name: "Joao", email: "joao@joao.com", password: "123456"}
+
+      response = User.changeset(params)
+
+      assert %Ecto.Changeset{
+              valid?: true,
+              changes: %{email: "joao@joao.com", password: "123456"},
+              errors: []
+            } = response
+    end
+
+    test "when there are a invalid params, returns a invalid changeset" do
+      params = %{name: "J", email: "joao@joao.com"}
+
+      response = User.changeset(params)
+
+      expected_response = %{
+        name: ["should be at least 2 character(s)"], password: ["can't be blank"]
+      }
+
+      assert errors_on(response) == expected_response
+    end
+  end
+end
---
+++ test/wabanex_web/schema_test.exs
@@ -0,0 +1,63 @@
+defmodule WabanexWeb.SchemaTest do
+  use WabanexWeb.ConnCase, async: true
+
+  alias Wabanex.User
+  alias Wabanex.Users.Create
+
+  describe "users queries" do
+    test "when a valid id is given, returns the user", %{conn: conn} do
+      params = %{email: "joao2@joao.com", name: "Joao 2", password: "123456"}
+
+      {:ok, %User{id: user_id}} = Create.call(params)
+
+      query = """
+        {
+          getUser(id: "#{user_id}"){
+            name
+            email
+          }
+        }
+      """
+
+      response =
+        conn
+        |> post("/api/graphql", %{query: query})
+        |> json_response(:ok)
+
+      expected_response = %{
+        "data" => %{
+          "getUser" => %{
+            "email" => "joao2@joao.com",
+            "name" => "Joao 2"
+          }
+        }
+      }
+
+      assert response == expected_response
+    end
+  end
+
+  describe "users mutations" do
+    test "when all params are valid, creates the user", %{conn: conn} do
+
+      mutation = """
+        mutation {
+          createUser(input: {
+            name: "Joao 3", email: "joao3@joao.com", password: "123456"
+          }){
+            id
+            name
+          }
+        }
+      """
+
+      response =
+        conn
+        |> post("/api/graphql", %{query: mutation})
+        |> json_response(:ok)
+
+      assert %{"data" => %{"createUser" => %{"id" => _id, "name" => "Joao 3"}}} = response
+    end
+  end
+
+end
